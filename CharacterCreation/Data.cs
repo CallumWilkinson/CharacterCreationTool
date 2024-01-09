c82716208639a32fe80f39e03dc515bdf85fac65
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-01-09 22:13:45 +1100

    added loadfata function for csv

---
+++ CharacterCreation/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreation
+{
+    internal class Data
+    {
+        public void LoadData() 
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Spells.xlsx");    
+            using (var reader = new StreamReader(filePath))
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    // Add your test logic here
+                }
+        }
+
+    }
+}
